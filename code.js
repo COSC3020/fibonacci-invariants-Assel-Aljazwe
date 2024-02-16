a23c172b5f20dc38ad2c33be8838fb629f1d0ff7
Assel-Aljazwe <157559559+Assel-Aljazwe@users.noreply.github.com>
2024-02-15 20:08:56 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,12 @@
 function fib(n) {
-    return n;
+    // Base Cases:
+    if (n === 0) 
+        return [0];
+    if (n === 1) 
+        return [0, 1];
+
+    // Recursive case
+    let sequence = fib(n - 1);
+    sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+    return sequence;
 }
